1cfae2aaf875a540dda598686ea2a4654fd21c80
Afomia Tadesse <afomiat79@gmail.com>
2024-11-16 11:27:28 -0800

    Added /name route with full name

--- main.go
+++ main.go
@@ -1,2 +1,19 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/name", func(c *gin.Context) {
+		c.String(http.StatusOK, "Afomia Tadesse")
+	})
+
+
+	r.Run(":8080")
+}
+
